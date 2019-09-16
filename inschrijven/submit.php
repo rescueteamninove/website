8ecd410f9f7695f8a335f322f6becbc8d82cbeee
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-09-17 01:01:14 +0200

    Commit on 2019-09-17 01:01:14

--- inschrijven/submit.php
+++ inschrijven/submit.php
@@ -16,6 +16,11 @@ $id_to_activity = array(
         "price" => 300,
         ),
 
+    "bs0" => array(
+        "description" => "Bijscholing Hoger Redder 28/12/2019",
+        "price" => 50,
+        ),
+
 );
 
 class Enroll {
